869ede1eb7c9fd5e54ac766c2c9b1f807bdf8899
abouraicha <abouraicha@gmail.com>
2022-04-26 15:59:39 +0000

    Fetching

--- src/components/Article/Fetching.js
+++ src/components/Article/Fetching.js
@@ -11,16 +11,180 @@ export const Fetching = (props) => {
     switch (type) {
       case "type2":
         return renderType2();
-        break;
       case "type3":
         return renderType3();
-        break;
       case "type4":
         return renderType4();
-        break;
+      case "type5":
+        return renderType5();
+      case "type6":
+        return renderType6();
+      case "type7":
+        return renderType7();
+      case "type8":
+        return renderType8();
       default:
         return renderDefault();
     }
+  };
+  const renderType8 = () => {
+    return (
+      <div className={`${styles.articleTemplate8} ${styles[size]}`}>
+        <div className={styles.articleContainer}>
+          <div className={`${styles.contentImg}`}></div>
+          <div className={styles.content}>
+            <div style={{ flex: "1", width: "100%" }}>
+              <Skeleton width={100} height={25} />
+              <div style={{ display: "flex", marginTop: "10px" }}>
+                <Skeleton width={80} height={25} />
+                &nbsp;
+                <Skeleton width={40} height={25} />
+              </div>
+              <div style={{ width: "100%", marginTop: "20px" }}>
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+              </div>
+            </div>
+            <div className={styles.userActions} style={{ marginTop: "15px" }}>
+              {renderAvatar()}
+              <hr />
+              <div className={styles.actions}>
+                <Skeleton width={30} height={24} />
+                &nbsp;
+                <Skeleton width={30} height={24} />
+                <div style={{ marginLeft: "auto" }}>
+                  <Skeleton width={80} height={24} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderType7 = () => {
+    return (
+      <div className={`${styles.articleTemplate7} ${styles[size]}`}>
+        <div className={styles.articleContainer}>
+          <div
+            style={{
+              padding: "10px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ flex: "1", width: "100%" }}>
+              <Skeleton width={100} height={25} />
+            </div>
+            <div className={styles.userActions}>
+              <div style={{ display: "flex", marginBottom: "20px" }}>
+                <Skeleton width={80} height={25} />
+                &nbsp;
+                <Skeleton width={40} height={25} />
+              </div>
+              <div style={{ width: "100%", marginBottom: "40px" }}>
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+              </div>
+
+              {renderAvatar()}
+              <hr />
+              <div className={styles.actions}>
+                <Skeleton width={30} height={24} />
+                &nbsp;
+                <Skeleton width={30} height={24} />
+                <div style={{ marginLeft: "auto" }}>
+                  <Skeleton width={80} height={24} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderType6 = () => {
+    return (
+      <div className={`${styles.articleTemplate6} ${styles[size]}`}>
+        <div className={styles.articleContainer}>
+          <div className={styles.content}>
+            <div style={{ flex: "1", width: "100%" }}>
+              <Skeleton width={100} height={25} />
+              <div style={{ display: "flex", marginTop: "100px" }}>
+                <Skeleton width={80} height={25} />
+                &nbsp;
+                <Skeleton width={40} height={25} />
+              </div>
+              <div style={{ width: "100%", marginTop: "20px" }}>
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+                <Skeleton width="100%" height={20} />
+              </div>
+              <div style={{ width: "100%", marginTop: "20px" }}>
+                <Skeleton width="100%" height={15} />
+                <Skeleton width="100%" height={15} />
+                <Skeleton width="100%" height={15} />
+                <Skeleton width="100%" height={15} />
+              </div>
+            </div>
+            <div className={styles.userActions} style={{ marginTop: "15px" }}>
+              {renderAvatar()}
+              <hr />
+              <div className={styles.actions}>
+                <Skeleton width={30} height={24} />
+                &nbsp;
+                <Skeleton width={30} height={24} />
+                <div style={{ marginLeft: "auto" }}>
+                  <Skeleton width={80} height={24} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.contentImg}`}>
+            <Skeleton width="100%" height="100%" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderType5 = () => {
+    return (
+      <div className={`${styles.articleTemplate5} ${styles[size]}`}>
+        <div className={styles.articleContainer}>
+          <div className={styles.contentImg}>
+            <Skeleton width="100%" height="100%" />
+          </div>
+          <div className={styles.content}>
+            <Skeleton width={100} height={25} />
+            <div style={{ width: "100%", marginTop: "20px" }}>
+              <Skeleton width="100%" height={20} />
+              <Skeleton width="100%" height={20} />
+              <Skeleton width="100%" height={20} />
+            </div>
+
+            {renderAvatar()}
+
+            <div className={styles.userActions} style={{ marginTop: "15px" }}>
+              <hr />
+              <div className={styles.actions}>
+                <Skeleton width={30} height={24} />
+                &nbsp;
+                <Skeleton width={30} height={24} />
+                <div style={{ marginLeft: "auto" }}>
+                  <Skeleton width={80} height={24} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   const renderType4 = () => {
