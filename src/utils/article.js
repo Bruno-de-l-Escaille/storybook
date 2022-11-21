db72c8b48cc7062c6dd7bd542ad2eb0f063d26fa
AyoubTougui <Mr.Tougui.Ayoub@gmail.com>
2022-11-21 17:22:42 +0100

    Add chain url

--- src/utils/article.js
+++ src/utils/article.js
@@ -158,6 +158,25 @@ export const getAlbum = ({ albums }) => {
   return medias;
 };
 
+export const sanitize = (str) => {
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+  var to = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-"); // collapse dashes
+
+  return str;
+};
+
 export const getAuthors = ({ author, chains }, lng = "fr") => {
   let authors = [];
   let others = [];
@@ -186,13 +205,16 @@ export const getAuthors = ({ author, chains }, lng = "fr") => {
           }
         });
       }
-
+      const chainName =
+        chain[`name${lng.charAt(0).toUpperCase()}${lng.slice(1)}`];
+      let chainUrl = `/${lng}/chain/${sanitize(chainName)}/${chain.id}`;
       return {
         id: chain.id,
         name: chain[nameAttr],
         headline: chain.headline,
         avatar: mediaChain ? mediaChain.avatar : "",
         avatarUrl: mediaChain ? mediaChain.avatarUrl : "",
+        url: chainUrl,
       };
     });
   }
