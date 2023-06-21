8c6bfc7fbb533679e865ebfbab551e32d77a4f64
Hicham <hic.mait@gmail.com>
2023-06-21 17:02:18 +0100

    editor

--- src/components/TTPEditor/TTPEditor.js
+++ src/components/TTPEditor/TTPEditor.js
@@ -71,7 +71,7 @@ export const TTPEditor = ({ auth, env, lng, initialContent, setContent }) => {
         setOptions={{
           allowedClassNames: "^se-|__se__|katex|ttp-|author|article-embed",
           attributesWhitelist: {
-            all: "style|data-.+",
+            all: "*",
             input: "checked",
           },
           height: "auto",
