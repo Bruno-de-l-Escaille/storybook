f86920f5c0f5e89a9f56422f70f1de6adf1c57b4
Hajar Elma <hajarelmadhoun@gmail.com>
2024-04-02 16:22:03 +0000

    popup confirm

---
+++ src/components/PopUp/PopupConfirm.stories.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
+import StoryRouter from "storybook-react-router";
+
+import { PopupConfirm } from "./PopupConfirm";
+
+export default {
+  title: "PopupConfirm",
+  component: PopupConfirm,
+  decorators: [
+    StoryRouter(),
+    (story) => <div style={{ padding: "3rem" }}>{story()}</div>,
+    withKnobs,
+  ],
+};
+
+export const Lock = () => (
+  <PopupConfirm
+    type="lock"
+    isOpen={boolean("isOpen", true)}
+    onCancel={() => console.log("cancel")}
+    onConfirm={() => console.log("handleLock")}
+    inProcess={boolean("inProcess", false)}
+    actionFailed={boolean("actionFailed", false)}
+    text="Êtes-vous sûr de vouloir verrouiller cette période "
+    labelNo="Non, annuler"
+    labelYes="Oui, verrouiller"
+    labelError="Error"
+    //period={period}
+  />
+);
+
+export const Unlock = () => (
+  <PopupConfirm
+    type="unlock"
+    isOpen={true}
+    onCancel={() => console.log("cancel")}
+    onConfirm={() => console.log("handleUnlock")}
+    inProcess={false}
+    actionFailed={false}
+    text="Voulez-vous vraiment déverrouiller cette période "
+    labelNo="Non, annuler"
+    labelYes="Oui, déverrouiller"
+    labelError="Error"
+    //period={period}
+  />
+);
