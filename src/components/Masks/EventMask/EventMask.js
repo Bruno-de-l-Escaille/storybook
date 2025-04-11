b23fd8ea2d82277c4a7612e46e715ff792d6d4f6
HindAT <hindaittaleb2002@gmail.com>
2025-04-11 21:27:32 +0100

    feat: Add watch configuration functionality to EventMask

--- src/components/Masks/EventMask/EventMask.js
+++ src/components/Masks/EventMask/EventMask.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import { Mask } from "../Common/Mask/Mask";
 import IconHash from "../Assets/IconHash";
 import IconCalendarWhite from "../Assets/IconCalendarWhite";
-import { parseJson } from "../../../utils";
+import { getApiUrl, parseJson } from "../../../utils";
 import { FocusForm } from "../Common/FocusForm/FocusForm";
 import TagsForm from "../Common/TagForm/TagsForm";
+import { updateEventWatchConfig } from "../../../api";
+import { Toast } from "../../ToastContainer/ToastContainer";
+import { I18N } from "../../../i18n";
 
 export const EventMask = ({
   event,
@@ -15,6 +18,34 @@ export const EventMask = ({
   ...restProps
 }) => {
   const [activeForm, setActiveForm] = useState(null);
+  const [watchConfig, setWatchConfig] = useState(
+    event.watchConfig === 1 ? true : false
+  );
+  const iconUrl = "	https://unitedassociates.be/img/apps/offfcourse.svg";
+
+  const handleWatchConfigChange = (newWatchConfig) => {
+    const id = event.id;
+    const updatedWatchConfig = newWatchConfig ? 1 : 0;
+    const apiUrl = getApiUrl(env);
+
+    updateEventWatchConfig({ id, apiUrl, updatedWatchConfig, token })
+      .then(({ data }) => {
+        const updatedEvent = data.data;
+        setWatchConfig(updatedEvent.watchConfig);
+
+        Toast.success(
+          newWatchConfig
+            ? I18N[language]["watchAdded"]
+            : I18N[language]["watchRemoved"]
+        );
+      })
+      .catch((error) => {
+        Toast.error(
+          error.response?.data?.message ||
+            I18N[language]["errorUpdatingWatchConfig"]
+        );
+      });
+  };
 
   const toggleForm = (form) => {
     if (activeForm === form) {
@@ -74,6 +105,26 @@ export const EventMask = ({
         {
           children: <IconCalendarWhite width="17px" height="20px" />,
           onClick: () => toggleForm("focus"),
+        },
+        {
+          children: (
+            <div
+              style={{
+                marginLeft: "12%",
+                marginTop: "15%",
+              }}
+            >
+              <img
+                src={iconUrl}
+                style={{
+                  maxHeight: "100%",
+                  maxWidth: "100%",
+                  opacity: !watchConfig ? "0.2" : 1,
+                }}
+              />
+            </div>
+          ),
+          onClick: () => handleWatchConfigChange(!watchConfig),
         },
       ]}
       renderForm={renderForm}
